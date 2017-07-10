8f250b72535cbc714ccf7acfe9a68687add64412
mateoszym <mateoszym@gmail.com>
2017-07-10 12:54:45 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var allButtons = document.getElementsByClassName('button');
 
-for ( var i = 0; i < allButtons.length; i++) {
+for (var i = 0; i < allButtons.length; i++) {
 	console.log(allButtons[i].innerText);
 }
